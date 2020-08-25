697688719ffae41c4c9267d0cf97454062bc3477
christin <christin@wednesday.is>
2020-08-25 11:19:04 +0530

    Add pre-commit local hook
    Signed-off-by: christin <christin@wednesday.is>

--- cmd/install-lint-dependencies/install.sh
+++ cmd/install-lint-dependencies/install.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Installing pre-commit dependencies"
+echo "--Installing lint dependencies---"
 
 go get github.com/fzipp/gocyclo
 go get golang.org/x/tools/cmd/goimports
 GO111MODULE=on go get -v -u github.com/go-critic/go-critic/cmd/gocritic
 go get -u golang.org/x/lint/golint
 
+set -e
+
+gofmt -d -w pkg/
+go vet
+golint -set_exit_status /...
+goimports pkg/
+go mod tidy
+git add --all
+
 if [ "$?" = "0" ]; then
-    echo "Dependencies Installed."
+    echo "---Dependencies Installed---"
 else 
-    echo "Installation Failed."
+    echo "---Installation Failed---"
     exit 1
 fi
