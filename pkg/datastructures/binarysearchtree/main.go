887fe208ecb67e0c3724ae1c382420c1a59a8dd5
christin <christin@wednesday.is>
2020-09-08 22:58:37 +0530

    Update renamed key to searchKey
    Signed-off-by: christin <christin@wednesday.is>

--- pkg/datastructures/binarysearchtree/main.go
+++ pkg/datastructures/binarysearchtree/main.go
@@ -8,62 +8,63 @@ type (
 		Insert(data)
 		Search(int) (string, error)
 	}
-	// BinaryNode is a struct 
+	// BinaryNode is a struct
 	BinaryNode struct {
 		data
-		leftChild *BinaryNode
+		leftChild  *BinaryNode
 		rightChild *BinaryNode
 	}
 	data struct {
-		key int
+		key  int
 		name string
 	}
 )
 
- // Search in the binary tree for a name with the key
-func (node *BinaryNode) Search(key int) (name string,err  error) {
+// Search in the binary tree for a name with the key
+func (node *BinaryNode) Search(searchKey int) (name string, err error) {
 	err = nil
 	name = ""
 	switch {
-		case node.data.key == key:
-			name = node.data.name
-			return name, err
-		case node.data.key < key:
-			if node.rightChild.data.name != "" {
-				return node.rightChild.Search(key)
-			}
-			err = fmt.Errorf("Not found")
-			return name, err
-		case node.data.key > key:
-			if node.leftChild.data.name != "" {
-				return node.leftChild.Search(key)
-			}
-			err = fmt.Errorf("Not found")
-			return name, err
+	case node.data.key == searchKey:
+		name = node.data.name
+		return name, err
+	case node.data.key < searchKey:
+		if node.rightChild.data.name != "" {
+			return node.rightChild.Search(searchKey)
+		}
+		err = fmt.Errorf("Not found")
+		return name, err
+	case node.data.key > searchKey:
+		if node.leftChild.data.name != "" {
+			return node.leftChild.Search(searchKey)
+		}
+		err = fmt.Errorf("Not found")
+		return name, err
 	}
 	return
 }
+
 // Insert data into a node according to key
 func (node *BinaryNode) Insert(input data) {
 	switch {
-		case node == nil || node.data.name == "":
-			// node is empty
-			node.data = input
-		case node.data.key > input.key:
-			// key less than node, save data in the left branch
-			if node.leftChild == nil {
-				// empty left child
-				node.leftChild = &BinaryNode{input, nil, nil}
-				} else {
-				node.leftChild.Insert(input)
-			}
-		case node.data.key < input.key:
-			// key greater than node key, save data in the right branch
-			if node.rightChild == nil {
-				// empty left child
-				node.rightChild = &BinaryNode{input, nil, nil}
-			} else {
-				node.rightChild.Insert(input)
-			}
+	case node == nil || node.data.name == "":
+		// node is empty
+		node.data = input
+	case node.data.key > input.key:
+		// key less than node, save data in the left branch
+		if node.leftChild == nil {
+			// empty left child
+			node.leftChild = &BinaryNode{input, nil, nil}
+		} else {
+			node.leftChild.Insert(input)
+		}
+	case node.data.key < input.key:
+		// key greater than node key, save data in the right branch
+		if node.rightChild == nil {
+			// empty left child
+			node.rightChild = &BinaryNode{input, nil, nil}
+		} else {
+			node.rightChild.Insert(input)
+		}
 	}
 }
