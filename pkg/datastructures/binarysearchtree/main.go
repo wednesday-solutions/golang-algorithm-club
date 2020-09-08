ea59b1ccefaadb42e3917953f4703f9a54b5d474
christin <christin@wednesday.is>
2020-09-08 22:35:46 +0530

    Add binary search tree datastructure Add tests for binary search tree Insert and Search methods
    Signed-off-by: christin <christin@wednesday.is>

---
+++ pkg/datastructures/binarysearchtree/main.go
@@ -0,0 +1,72 @@
+package binarysearchtree
+
+import "fmt"
+
+type (
+	// BinarySearchTree implements Insert and Search methods for a BinaryNode
+	BinarySearchTree interface {
+		Insert(data)
+		Search(int) (string, error)
+	}
+	// BinaryNode is a struct 
+	BinaryNode struct {
+		data
+		leftChild *BinaryNode
+		rightChild *BinaryNode
+	}
+	data struct {
+		key int
+		name string
+	}
+)
+
+ // Search in the binary tree for a name with the key
+func (node *BinaryNode) Search(key int) (name string,err  error) {
+	err = nil
+	name = ""
+	switch {
+		case node.data.key == key:
+			name = node.data.name
+			return name, err
+		case node.data.key < key:
+			if node.rightChild.data.name != "" {
+				return node.rightChild.Search(key)
+			}
+			err = fmt.Errorf("Not found")
+			return name, err
+		case node.data.key > key:
+			if node.leftChild.data.name != "" {
+				return node.leftChild.Search(key)
+			}
+			err = fmt.Errorf("Not found")
+			return name, err
+	}
+	return
+}
+// Insert data into a node according to key
+func (node *BinaryNode) Insert(d data) {
+	switch {
+		case node == nil || node.data.name == "":
+			// node is empty
+			node.data = d
+			break
+		case node.data.key > d.key:
+			// key less than node, save data in the left branch
+			if node.leftChild == nil {
+				// empty left child
+				node.leftChild = &BinaryNode{d, nil, nil}
+				} else {
+				node.leftChild.Insert(d)
+			}
+			break
+		case node.data.key < d.key:
+			// key greater than node key, save data in the right branch
+			if node.rightChild == nil {
+				// empty left child
+				node.rightChild = &BinaryNode{d, nil, nil}
+			} else {
+				node.rightChild.Insert(d)
+			}
+			break
+	}
+}
