a709e8137d54f95264cba313314b4d0b1f539a4b
christin <christin@wednesday.is>
2020-10-06 22:15:35 +0530

    fix: installed precommit
    Signed-off-by: christin <christin@wednesday.is>

--- pkg/datastructures/hashtable/main.go
+++ pkg/datastructures/hashtable/main.go
@@ -1,20 +1,20 @@
 package hashtable
 
 type (
-	 item struct {
-		next *item 
-		key string
+	item struct {
+		next  *item
+		key   string
 		value string
 	}
 	bucket struct {
-		node *item
+		node   *item
 		length int
 	}
 )
 
 var table = make([]*bucket, 100)
 
-// hash function, adds up the rune values of the characters in the key 
+// hash function, adds up the rune values of the characters in the key
 func hash(key string) int {
 	sum := 0
 	for _, v := range key {
@@ -24,31 +24,31 @@ func hash(key string) int {
 	return sum
 }
 
-func getItemByKey (key string, bucketItem *item) *item {
-	if	bucketItem.key == key {
+func getItemByKey(key string, bucketItem *item) *item {
+	if bucketItem.key == key {
 		return bucketItem
 	}
 	return getItemByKey(key, bucketItem.next)
 }
 
-// GetValueFromBucket get the value of the key 
+// GetValueFromBucket get the value of the key
 func GetValueFromBucket(key string) string {
-	index:= hash(key)
-	bucketItem:= table[index].node
-	item:= getItemByKey(key, bucketItem)
+	index := hash(key)
+	bucketItem := table[index].node
+	item := getItemByKey(key, bucketItem)
 	return item.value
 }
 
 // Insert a key value pair into the table
 func Insert(key string, value string) {
-	indexToInsert:= hash(key)
-	newItem := item {nil, key, value}
+	indexToInsert := hash(key)
+	newItem := item{nil, key, value}
 	if table[indexToInsert] == nil {
 		// new entry
-		newBucket := bucket {&newItem, 1}
+		newBucket := bucket{&newItem, 1}
 		table[indexToInsert] = &newBucket
-	} else  {
-		// add next linked item to the bucket
+	} else {
+		// add next free linked item to the bucket
 		table[indexToInsert].node.next = &newItem
 		table[indexToInsert].length++
 	}
