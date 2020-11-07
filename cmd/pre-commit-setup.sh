3efc4596a0fd298cdea1b32f7306484d744a7b45
christin <christin@wednesday.is>
2020-11-08 01:31:16 +0530

    update(precommit): fixed shell script for pre-commit setup
    added check for pre-commit installation
    
    added wait for installation
    
    ctrl-c handler
    
    graceful error handling
    
    Signed-off-by: christin <christin@wednesday.is>

--- cmd/pre-commit-setup.sh
+++ cmd/pre-commit-setup.sh
@@ -1,10 +1,31 @@
-#!/usr/bin/env bash
 # Install pre-commit and required dependencies.
+set -m
 set -e
 
-echo "--Installing pre-commit & dependencies---"
-brew install pre-commit
-pre-commit install
+function handle_exit(){
+    kill $!
+    exit 2
+}
+
+trap "handle_exit" SIGINT
+
+brew info pre-commit | grep -i "bottled"
+
+if [[ $? != "0" ]]; then
+    echo "Installing pre-commit"
+    brew install pre-commit
+    INSTALL_PRECOMMIT=$(jobs -p)
+fi
+
+wait $INSTALL_PRECOMMIT
+if [ $? != "0" ]; then
+    echo "--- setup failed ---"
+    exit 1
+else
+    pre-commit install
+fi
+
+echo "--Installing dependencies---"
 GO111MODULE=off go get github.com/fzipp/gocyclo
 GO111MODULE=off go get golang.org/x/tools/cmd/goimports
 GO111MODULE=off go get -v -u github.com/go-critic/go-critic/cmd/gocritic
@@ -17,3 +38,5 @@ else
     echo "--- setup failed ---"
     exit 1
 fi
+
+exit 0
